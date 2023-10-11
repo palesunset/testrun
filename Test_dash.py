0cbc16eac82a2d0f35e106910e89e50d29e4746a
palesunset <146456469+palesunset@users.noreply.github.com>
2023-10-12 02:15:18 +0800

    Update Test_dash.py

--- Test_dash.py
+++ Test_dash.py
@@ -79,7 +79,7 @@ def generate_sankey_chart(df):
             color=[link['color'] for link in links]
         )
     )
-    st.plotly_chart(go.Figure(data=[data], layout=go.Layout(height=1000, width=1700)))
+    st.plotly_chart(go.Figure(data=[data], layout=go.Layout(height=1000, width=1600)))
 
 
 # --------------- 1. Title, Uploaders, and Predefined Values ---------------
